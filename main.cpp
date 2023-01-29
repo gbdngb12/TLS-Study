1c05f2ca8db4e0dc1873cf02a2a0dd11a50617b4
gbdngb12 <gbdngb12@naver.com>
2023-01-29 13:13:45 +0900

    add test case

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 TEST_CASE("test name") {
     vector<int> a, b;
+    int c, d;
 
     a.push_back(4);
     a.push_back(5);
@@ -13,5 +14,7 @@ TEST_CASE("test name") {
     b.push_back(5);
     b.push_back(4);
 
+    c = 3;
+    d = 3;
     REQUIRE(equal(a.begin(), a.end(), b.begin()));
 }
