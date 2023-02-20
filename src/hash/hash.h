2fe68bd0cb4a789a22834f1408ac57d1cd938a7f
gbdngb12 <gbdngb12@naver.com>
2023-02-20 14:34:43 +0900

    implementation PRF

--- src/hash/hash.h
+++ src/hash/hash.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <arpa/inet.h>
 #include <nettle/sha2.h>
-#include <valarray>
+
 #include <array>
 #include <cstdint>
+#include <valarray>
 #include <vector>
 
 namespace HASH {
@@ -70,34 +71,35 @@ class HMAC {
     template <typename It>
     void key(const It begin, const It end) {
         int length = end - begin;
-        //key, o_key_pad, i_key_pad 연산을 위한 상수 및 저장공간
+        // key, o_key_pad, i_key_pad 연산을 위한 상수 및 저장공간
         std::valarray<unsigned char> key((int)0x00, H::block_size), out_xor(0x5c, H::block_size), in_xor(0x36, H::block_size);
 
         // block_size보다 크면 해쉬, 작으면 0패딩
-        if(length > H::block_size) {
+        if (length > H::block_size) {
             auto h = sha_.hash(begin, end);
-            for(int i = 0; i < H::output_size; i++) {
+            for (int i = 0; i < H::output_size; i++) {
                 key[i] = h[i];
             }
-        } else if(int i = 0; length <= H::block_size) {//c++17 initializer 
-            //0 padiing
-            for(auto it = begin; it != end; it++) {
+        } else if (int i = 0; length <= H::block_size) {  // c++17 initializer
+            // 0 padiing
+            for (auto it = begin; it != end; it++) {
                 key[i++] = *it;
             }
         }
-        //o_key_pad 연산, i_key_pad 연산
+        // o_key_pad 연산, i_key_pad 연산
         o_key_pad_ = key ^ out_xor;
         i_key_pad_ = key ^ in_xor;
     }
 
-    template<typename It> auto hash(const It begin, const It end) {
+    template <typename It>
+    auto hash(const It begin, const It end) {
         std::vector<unsigned char> v;
-        //hash(i_key_pad | message)
+        // hash(i_key_pad | message)
         v.insert(v.begin(), std::begin(i_key_pad_), std::end(i_key_pad_));
         v.insert(v.end(), begin, end);
         auto h = sha_.hash(v.begin(), v.end());
 
-        //hash(o_key_pad | hash(i_key_pad | message))
+        // hash(o_key_pad | hash(i_key_pad | message))
         v.clear();
         v.insert(v.begin(), std::begin(o_key_pad_), std::end(o_key_pad_));
         v.insert(v.end(), h.begin(), h.end());
@@ -107,5 +109,71 @@ class HMAC {
    protected:
     H sha_;  // SHA Hash Family
     std::valarray<unsigned char> o_key_pad_, i_key_pad_;
+};
+
+template <class H>
+class PRF {  // H is Hash Function
+   public:
+    /*!
+    @brief 확장하고자 하는 secret값을 설정한다
+    @param begin secret Iterator 시작
+    @param end secret Iterator 끝
+    */
+    template <class It>
+    void secret(const It begin, const It end) {
+        hmac_.key(begin, end);
+    }
+
+    /*!
+    @brief PRF label값을 설정한다.
+    @param p const char *label
+    */
+    void label(const char *p) {
+        label_.clear();
+        while (*p) label_.push_back(*p++);
+    }
+
+    /*!
+    @brief PRF seed값을 설정한다.
+    @param begin seed Iterator 시작
+    @param end seed Iterator 끝
+    */
+    template <class It>
+    void seed(const It begin, const It end) {
+        seed_.clear();
+        for (It it = begin; it != end; it++) {
+            seed_.push_back(*it);
+        }
+    }
+
+    /*!
+    @brief 랜덤 n바이트로 확장한다.
+    @param n 확장할 바이트 수
+    @return 확장한 n바이트 vector
+    */
+    std::vector<unsigned char> expand_n_byte(int n) {
+        // seed = label + seed_
+        auto seed = label_;
+        seed.insert(seed.end(), seed_.begin(), seed_.end());  // A(0) = seed
+
+        std::vector<unsigned char> r, v;
+
+        for (auto A = hmac_.hash(seed.begin(), seed.end());    // A(1) = HMAC(secret, seed)
+             r.size() < n;                                     // n바이트 보다 작게
+             A = hmac_.hash(A.begin(), A.end()), v.clear()) { // A(i) = HMAC(secret, A(i - 1))
+            v.insert(v.end(), A.begin(), A.end());//A(1)
+            v.insert(v.end(), seed.begin(), seed.end());//v = A(1) + seed
+            auto h = hmac_.hash(v.begin(), v.end());
+            r.insert(r.end(), h.begin(), h.end());// r = h(secret, A(1) + seed) + h(secret, A(2) + seed) + ... +
+        }
+        //크기가 작다면 0 padding
+        //크기가 크다면 자름
+        r.resize(n);//리턴할 벡터 크기 n바이트로 조절
+        return r;
+    }
+
+   protected:
+    HMAC<H> hmac_;
+    std::vector<unsigned char> label_, seed_;
 };
 }  // namespace HASH
--- test/cert_test.cpp
+++ test/cert_test.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include "util.h"
 #include "auth.h"
+#include "hash.h"
+
+using namespace std;
 
 TEST_CASE("base64") {
     std::string s = "aGVsbG8gd29ybGQ=";
@@ -36,5 +39,46 @@ TEST_CASE("certificate") {
     }
 
     auto jv = DER::der_to_json(ss);
-    std::cout << jv[0][0][6][1];
+    std::cout << jv[0][0][6][1] << std::endl;
 }
+
+TEST_CASE("Check Certificate Chain") {
+    std::ifstream f("../../server-cert.pem");
+    //첫번째 DER
+    string s = DER::get_certificate_core(f);
+    auto v = BASE64::base64_decode(s);
+
+    HASH::SHA2 sha;
+    int length = v[6] * 256/*v[6] << 8*/ + v[7] + 4;
+    //TBS Certificate를 해쉬한다.
+    auto arr = sha.hash(v.begin() + 4, v.begin() + 4 + length);
+    cout << "hash" << endl;
+    for(const auto& c : arr) {
+        cout << setw(2) << setfill('0') << hex<<static_cast<int>(c);
+    }
+    cout<<endl;
+
+    stringstream ss;
+    for(uint8_t c : v) {
+        ss << c;
+    }
+    auto jv = DER::der_to_json(ss);
+    auto [K, e, sign] = DER::get_pubkeys(jv);//인증서 1의 서명값을 가져온다.
+
+    //두번째 DER
+    s = DER::get_certificate_core(f);
+    v = BASE64::base64_decode(s);
+    stringstream ss2;
+    for(uint8_t c : v) {
+        ss2 << c;
+    }
+    jv = DER::der_to_json(ss2);
+    auto [K2, e2, sign2] = DER::get_pubkeys(jv);//인증서 2의 공개키값을 가져온다.
+
+    cout << "sign verify" << endl;
+    cout << UTIL::powm(sign, e2, K2);//인증서 1의 서명을 확인한다.
+
+}
+
+//374dbaf09c08e4df4c4eeb31ac1799676f39f4bc07993eeb10806bec72efca76
+//374dbaf09c08e4df4c4eeb31ac1799676f39f4bc07993eeb10806bec72efca76
